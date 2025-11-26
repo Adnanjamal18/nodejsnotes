bcfc0122e08b1d82a4d4c740330e1cfd53edb8c0
Adnan jamal <aj@marshee.com>
2025-11-26 21:06:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//!import statement
+const readline= require('readline'); //?library(module) for read input output
+const fs= require('fs'); //?(module)for reading and writing files 
+/*
+//!lecture 
+//? READING INPUT AND WRITING INPUT
+const ri = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+ri.question("please enter youn name:",(name)=>{
+    console.log("you entered:"+name);
+    ri.close();
+})
+
+ri.on('close',()=>{
+    console.log("Interface closed");
+    process.exit(0);
+})
+*/
