8b2556c1c9e689d4eec2af9ca8e4977da5643c69
Adnan jamal <aj@marshee.com>
2025-11-28 20:53:11 +0530

    second

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const readline= require('readline'); //?library(module) for read input output
 const fs= require('fs'); //?(module)for reading and writing files 
 /*
-//!lecture 
+//!lecture 1
 //? READING INPUT AND WRITING INPUT
 const ri = readline.createInterface({
     input: process.stdin,
@@ -19,3 +19,11 @@ ri.on('close',()=>{
     process.exit(0);
 })
 */
+//!ecture 2
+//?Reading and writing to a file
+let teztIn = fs.readFileSync('./Files/input.txt','utf-8');
+console.log(teztIn);
+
+let content = `Data read from input.txt: ${teztIn}\nDate Created ${new Date()}`;//!use backticks (``) else string interpulation will be treated as normal string and you wont be able to run Date function or others if you use ('')
+//* inside single quotes ('') ${...} is teated as normal text
+fs.writeFileSync('./Files/output.txt',content)
