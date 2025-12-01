0744c7610a77e62f4c002df61e876fc3331acd22
Adnan jamal <aj@marshee.com>
2025-12-01 23:00:47 +0530

    third

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ ri.on('close',()=>{
     process.exit(0);
 })
 */
-//!ecture 2
-//?Reading and writing to a file
-let teztIn = fs.readFileSync('./Files/input.txt','utf-8');
-console.log(teztIn);
+// //!ecture 2
+// //?Reading and writing to a file
+// let teztIn = fs.readFileSync('./Files/input.txt','utf-8');
+// console.log(teztIn);
 
-let content = `Data read from input.txt: ${teztIn}\nDate Created ${new Date()}`;//!use backticks (``) else string interpulation will be treated as normal string and you wont be able to run Date function or others if you use ('')
-//* inside single quotes ('') ${...} is teated as normal text
-fs.writeFileSync('./Files/output.txt',content)
+// let content = `Data read from input.txt: ${teztIn}\nDate Created ${new Date()}`;//!use backticks (``) else string interpulation will be treated as normal string and you wont be able to run Date function or others if you use ('')
+// //* inside single quotes ('') ${...} is teated as normal text
+// fs.writeFileSync('./Files/output.txt',content)
+
+//!ecture 3
