06d5d2583a249bab38c6129c6396f96469930e13
Adnan jamal <aj@marshee.com>
2025-12-07 19:08:57 +0530

    fifth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //!import statement
 const readline = require('readline'); //?library(module) for read input output
 const fs = require('fs'); //?(module)for reading and writing files 
+const http = require('http');
 /*
 //!lecture 1
 //? READING INPUT AND WRITING INPUT
@@ -42,22 +43,46 @@ ri.on('close',()=>{
 //  console.log('reading file 2...')//! this part gets executed on main thread 
 
 
-//!Lecture 4
-//on fs module we get read wrtite and more methods 
-//?readfile method reads file synchronously TO THIS METHOD WE PASS 3 ARGUMENTS FIRST IS PATH OF THE FILE 
-fs.readFile(`./Files/start.txt`, `utf-8`, (error1, data1/*first argumentn will carry returned error if there is any second will carry dara which is read*/
-) => {
-    console.log(data1);
-    fs.readFile(`./Files/${data1}.txt`, 'utf-8', (error2, data2) => {
-        console.log(data2);
+// //!Lecture 4
+// //on fs module we get read wrtite and more methods 
+// //?readfile method reads file synchronously TO THIS METHOD WE PASS 3 ARGUMENTS FIRST IS PATH OF THE FILE 
+// fs.readFile(`./Files/start.txt`, `utf-8`, (error1, data1/*first argumentn will carry returned error if there is any second will carry dara which is read*/
+// ) => {
+//     console.log(data1);
+//     fs.readFile(`./Files/${data1}.txt`, 'utf-8', (error2, data2) => {
+//         console.log(data2);
 
-        fs.readFile(`./Files/append.txt`, 'utf-8', (error3, data3) => {
-            console.log(data3);
-            fs.writeFile(`./Files/output.txt`, `${data2}\n\n${data3}\n\n date created= ${new Date()}`,
-                () => {
-                    console.log('file written successfully');
-                })
-        })
-    })
+//         fs.readFile(`./Files/append.txt`, 'utf-8', (error3, data3) => {
+//             console.log(data3);
+//             fs.writeFile(`./Files/output.txt`, `${data2}\n\n${data3}\n\n date created= ${new Date()}`,
+//                 () => {
+//                     console.log('file written successfully');
+//                 })
+//         })
+//     })
+// })
+// console.log('reading...');
+
+//!Lecture 5
+//? STEP ONE CREATE SERVER 
+//* WHEN A NEW REQUEST HITS THE SERVER THE CALL BACK FUNCTION RECIVES TWO THE REQUEST AND RESPONSE
+//! AND THIS SERVER METHOD IS ALSO GOING TO RETURN THE SERVER OBJECT SO WE ASSIGN IT TO A VARIABLE
+const server = http.createServer((request, response)=>{
+ response.end('hello from the server') //?ON RESPONSE YOU CAN CALL END METHOD TO SEND A TEXT RESPONSE OR ANY RESPONCE YOU WILL SE THIS WRITTEN THERE  
+//THIS CALL BACK FUNC WILL BE EXECUTED EVERY TIME A NEW REQ HITS THE SERVER  
+console.log(' A NEW REQUEST RECIVED'); 
+//console.log(response);
+});
+//? STEP 2 START THE SERVER 
+//! ON THIS SERVER OBJECT WE CAN CALL A METHOD CALLED LISTEN AND THIS LISTEN METHOD STARTS A NEW SERVER
+//! AND IT LISTENS TO THE NEW REQUESTS
+//? THIS LISTEN METHOD TAKES FEW PERAMETER AND THE FIRST PARAMETER IS PORT NUMBER WHERE THE APPLICATION IS RUNNING
+//? FOR THAT WE PASS 8000 THEN WE ALSO NEED TO SPACIFY THE HOST IN OUR CASE THE HOST IS GOING TO BE THE LOCAL HOST 
+//? AND FOR THE LOCAL HOST THE IP ADDRESS WILL BE 127.0.0.1
+//? AND THIRD PARAMETER IS A CALL BACK FUNCTION WHICH IS EXECUTED AS SOON AS THE SERVER ACTULLY STARTS LISTENING TO THE REQUEST
+
+server.listen(8000, '127.0.0.1',() => {
+   console.log('server has started') 
 })
-console.log('reading...');
+//! NOW IF YOU RUN URL FOR OUR APPLICATION IN BROWSER A MESSAGE "A NEW REQUEST IS RECIVED" WILL BE SHOWN
+//! AND URL IN OUR CASE WILL BE "127.0.0.1:8000" GO AHEAD RUN THIS RUN IN TWO BROWSER AN CALLBACK FUNCTION OF CREATING SERVER WILL EXECUTE TWICE
